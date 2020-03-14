ccf4a255a1138a111598a0f67fe3a9fe64dcecde
PeterSilva <peter@bsqt.homeip.net>
2020-03-14 17:19:56 -0400

    After much manual massage of the CCCC Table in pdf, now have a first try at a mapper from CCCC to WIS Centre names.

--- cccc_transpose.py
+++ cccc_transpose.py
@@ -13,11 +13,55 @@ CCCC, CCCC_replacement, description, country
 The CCCC value is printed twice to provide the placeholder column CCCC_replacement,
 which will gradually be replaced. 
 
+lots of manual massage to produce the initial json file.
 
 
 
 """
 debug=False
+
+import json
+
+with open("TableC1.json", 'r') as tc1:
+    TableC1 = json.load(tc1)
+
+def map_centre_from_desc( cccc, iso2country, desc ):
+
+    l=desc.lower().replace('/','_').replace('.',' ').replace('(','').replace(')','').replace(',','').replace("'","").replace('"',"").split()
+    if (l[0] == "call") or ( l[0] == '--'):
+       return iso2country + "/" + cccc.lower()
+    return iso2country + "/" + '_'.join(l)
+   
+
+
+def map_country( cccc, cd ):
+
+   if cd.startswith('--') or cd.startswith("WMO"):
+      return "unmatched"
+
+   for wmo2 in TableC1:
+       if cd == TableC1[wmo2][1]:
+          return TableC1[wmo2][0]
+
+   if debug : print( 'mapping: %s' % cd )
+
+   if "Antarctic" in cd : return "aq"
+   elif "Canada" in cd : return "ca"
+   elif "Indian Ocean" in cd : return "io"
+   elif "Europe" in cd or "EUMETSAT" in cd: return "europe"
+   elif "French Polynesia" in cd: return "pf"
+   elif "Faroe" in cd: return "dk"
+   elif "Gough" in cd: return "sh"
+   elif "Mariana" in cd : return "mp"
+   elif "Nauru" in cd : return "nr"
+   elif "Nigeria" in cd : return "ng"
+   elif "Réunion" in cd : return "re"
+   elif "Russia" in cd : return "ru"
+   elif "Helena" in cd : return "sh"
+   elif "U.S.A" in cd or "United States of America" in cd : return "us"
+
+   print( "no match found for: %s" % cd )     
+   return( "unmatched" )
 
 with open("CCCC_en.txt", 'r') as ccccfile:
   column=0
@@ -27,8 +71,8 @@ with open("CCCC_en.txt", 'r') as ccccfile:
   country = {}
   in_a_page=True
   for l in ccccfile.readlines():
-      if debug: print('l=%s, column=%d' % (l,column) )
-      if 'Country' in l:
+      if debug: print('row=%s, column=%d, len(l)=%d, l=%s' % (table_index, column, len(l), l) )
+      if 'Country Name' in l:
          if debug: print('found Country')
          in_a_page=True
          column=0 
@@ -36,37 +80,39 @@ with open("CCCC_en.txt", 'r') as ccccfile:
          cccc = {}
          desc = {}
          country = {}
-         blanks = {}
 
-      if in_a_page and len(l) > 3 :
-         if column==1:
-             if debug: print('assigning CCCC: %s' % l.strip() )
-             cccc[table_index]=l.strip()    
-             table_index += 1
-         elif column==2:
-             if debug: print('assigning desc: %s' % l.strip() )
-             desc[table_index]=l.strip()    
-             table_index += 1
-         elif column==3:
-             if debug: print('assigning country: %s' % l.strip() )
-             country[table_index]=l.strip()    
-             table_index += 1
-         else:
-             pass
+      elif len(l) > 1:
+          if in_a_page: 
+             if column==1:
+                 if debug: print('assigning CCCC: %s' % l.strip() )
+                 cccc[table_index]=l.strip()    
+                 table_index += 1
+             elif column==2:
+                 if debug: print('assigning desc: %s' % l.strip() )
+                 desc[table_index]=l.strip()    
+                 table_index += 1
+             elif column==3:
+                 if debug: print('assigning country: %s' % l.strip() )
+                 country[table_index]=l.strip()    
+                 table_index += 1
       else:
-         if debug: print('blank line, next column')
+         if debug: print('blank line, next column in_a_page=%s' % in_a_page )
          if in_a_page:
              if column < 3:
-                 if debug: print('blank line, next column %d ' % column)
                  column += 1
+                 if debug: print('blank line, column is now %d ' % column)
                  row_count=table_index
                  table_index=0
              else:
-                 if debug: print('end of page')
+                 if debug: print('end of page len(cccc)=%d, len(country)=%d, len(desc)=%d' % ( len(cccc), len(country), len(desc) ))
                  column = 0
-                 for i in range(0,row_count):
-                     try:
-                         print( "%d,%s,%s,%s,%s" % (i, cccc[i], cccc[i], desc[i], country[i] ) )
-                     except Exception as ex:
-                         print( "index issue: %s" % ex ) 
-            
+                 for i in range(0,len(cccc)):
+                     #if i >= len(cccc):
+                     #    print( "huh? i=%d > len(cccc)=%d" % ( i, len(cccc) ) ) 
+                     #    continue
+                     #if i >= len(country):
+                     #    print( "huh? i=%d > len(country)=%d" % ( i, len(country) ) ) 
+                     #    continue
+                     iso2country=map_country(cccc[i],country[i])
+                     wis_centre = map_centre_from_desc( cccc[i], iso2country, desc[i] )
+                     print( '"%s": { "centre":"%s", "description":"%s","country":"%s" },' % (cccc[i], wis_centre, desc[i], country[i] ) )
