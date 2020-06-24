cd2362ef781906e1f52fbe8f8908a03616a1f293
PeterSilva <peter@bsqt.homeip.net>
2020-06-24 08:35:52 -0400

    replacing air/upper with upper-air as per feedback from Antje.

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ print ( "%s" % __file__ )
 
 import GTStoWIS2
 
-topic_builder=GTStoWIS2.GTStoWIS2(debug=True,dump_tables=True)
+topic_builder=GTStoWIS2.GTStoWIS2(debug=True,dump_tables=False)
 
 with open( 'AHL_examples.txt', 'r' ) as headers:
     hh=headers.readlines()
